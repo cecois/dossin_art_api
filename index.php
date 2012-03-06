29ea478851fa5242c142d45a6f9a6d9a19f04714
Dennis Lazar <dennisklazar@gmail.com>
2012-03-05 23:30:56 -0500

    commit test

--- index.php
+++ index.php
@@ -76,7 +76,7 @@ $app->get('/artists/:artistid/works/:outform','getArtistWorks');
 $app->get('/dbfields/:outform','getFields');
 
 
-//DENNIS'S ADDITIONS
+//DENNIS'S ADDITIONS 2432432
  // Routes
  //  Artists
  //$app->get('/artists/:outform', 'getArtists');
